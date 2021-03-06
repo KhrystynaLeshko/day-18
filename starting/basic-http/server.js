11ef9d3f568b5623c60ced47c94db7bd55dff5be
André de Albuquerque <itstheandre@gmail.com>
2021-03-06 10:52:48 +0100

    Everybody was kung fu fighting

--- starting/basic-http/server.js
+++ starting/basic-http/server.js
@@ -1,6 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((request, response) => {
+  console.log("Request url:", request.url);
+  console.log("Type of request: ", request.method);
+
   if (request.url === "/") {
     response.write(`<h1>Welcome to the jungle</h1>`);
   }
